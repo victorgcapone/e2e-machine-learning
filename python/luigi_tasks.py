d3ce4ad8c8705d3ba38e4bfa9ede33ef7bbb87c6
Victor Capone <victor.capone@matera.com>
2019-02-18 15:33:00 -0300

    Addding DataCleaning luigi task

---
+++ python/luigi_tasks.py
@@ -0,0 +1,28 @@
+import luigi
+import pandas as pd
+
+"""
+This task cleans the data and prepares it so we can train our
+machine learning models
+"""
+class CleanData(luigi.ExternalTask):
+
+    def output(self):
+        return luigi.LocalTarget('data/clean_titanic_data.csv')
+
+    def run(self):
+        data = pd.read_csv("../datasets/titanic.tsv", sep="\t")
+        # Fill Age NaNs with mean value
+        data["Age"].fillna(data["Age"].mean(), inplace=True)
+        # Drop all other NaN value
+        data = data.dropna()
+        # Drop columns that we won't use
+        data = data.drop(["Name", "Cabin", "Ticket", "PassengerId"], axis=1)
+        # OneHotEncode this columns
+        data = pd.get_dummies(data, columns=["Embarked", "Sex"])
+        with self.output().open('w') as out:
+            data.to_csv(out)
+
+"""
+This tasks train the machine learning model and validates it 
+"""
