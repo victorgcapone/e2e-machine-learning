12aa9e5c8687217bd51e7942a9380c4b96fdc46c
Victor Capone <victor.capone@matera.com>
2019-02-19 10:05:45 -0300

    Changing dataset, network architeture and adding model persistence

--- python/luigi_tasks.py
+++ python/luigi_tasks.py
@@ -2,7 +2,8 @@ import luigi
 import pandas as pd
 import typing as tg
 from model import build_keras_model
-from sklearn.model_selection import GridSearchCV
+import h5py
+
 """
 This task cleans the data and prepares it so we can train our
 machine learning models
@@ -10,19 +11,19 @@ machine learning models
 class CleanData(luigi.ExternalTask):
 
     def output(self) -> luigi.LocalTarget:
-        return luigi.LocalTarget('data/clean_titanic_data.csv')
+        return luigi.LocalTarget('../data/clean_titanic_data.csv')
 
 
     def run(self) -> None:
-        data = pd.read_csv("../datasets/titanic.tsv", sep="\t")
+        data = pd.read_csv("../datasets/titanic.csv")
         # Fill Age NaNs with mean value
         data["Age"].fillna(data["Age"].mean(), inplace=True)
         # Drop columns that we won't use
-        data = data.drop(["Name", "Cabin", "Ticket", "PassengerId"], axis=1)
+        data = data.drop(["Embarked", "Name", "Cabin", "Ticket", "PassengerId"], axis=1)
         # Drop all other NaN value
         data = data.dropna()
         # OneHotEncode this columns
-        data = pd.get_dummies(data, columns=["Embarked", "Sex"])
+        data = pd.get_dummies(data, columns=["Pclass", "Sex"], drop_first=True)
         with self.output().open('w') as out:
             data.to_csv(out, index=False)
 
@@ -33,8 +34,8 @@ class TrainAndTestData(luigi.Task):
 
     def output(self) -> tg.Dict[str, luigi.LocalTarget]:
         # Not recomended, but this is just an example, ideally we would split the task in two and retur a single Target
-        return {"train" : luigi.LocalTarget("data/train_data.csv"),
-                "test" : luigi.LocalTarget("data/test_data.csv")}
+        return {"train" : luigi.LocalTarget("../data/train_data.csv"),
+                "test" : luigi.LocalTarget("../data/test_data.csv")}
 
 
     def requires(self) -> luigi.Task:
@@ -46,20 +47,21 @@ class TrainAndTestData(luigi.Task):
             data = pd.read_csv(input_file)
         train_data = data.sample(frac=0.7)
         test_data = data.drop(train_data.index)
-        print(data)
-        print(train_data)
-        print(test_data)
         with self.output()["train"].open("w") as train_out:
             train_data.to_csv(train_out, index=False)
         with self.output()["test"].open("w") as test_out:
             test_data.to_csv(test_out, index=False)
+
+
+
 """
 This tasks train the machine learning model and validates it using the training and test data
 """
 class TrainModel(luigi.Task):
 
     def output(self) -> luigi.LocalTarget:
-        return luigi.LocalTarget("model/ml_model")
+        return {"model" : luigi.LocalTarget("../model/model.yaml"),
+                "weights" : luigi.LocalTarget("../model/weights.h5")}
 
 
     def requires(self) -> luigi.Task:
@@ -67,8 +69,7 @@ class TrainModel(luigi.Task):
 
 
     def run(self) -> None:
-        model = build_keras_model(epochs=500)
-        print(self.input())
+        model = build_keras_model(epochs=100, batch_size=1)
         with self.input()["train"].open('r') as train:
             training_data = pd.read_csv(train)
         x_train = training_data.drop("Survived", axis=1)
@@ -80,3 +81,7 @@ class TrainModel(luigi.Task):
         y_test = test_data.Survived
         score = model.score(x_test.values, y_test.values)
         print(score)
+        with self.output()['model'].open('w') as model_out:
+            model_out.write(model.model.to_yaml())
+        # Keras won't let us use a buffer as a parameter :c
+        model.model.save_weights(self.output()['weights'].path)
--- python/model.py
+++ python/model.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(32, input_dim=10, activation=tf.nn.relu),
-    tf.keras.layers.Dense(32, activation=tf.nn.relu),
-    tf.keras.layers.Dense(2, activation=tf.nn.softmax)
+    tf.keras.layers.Dense(32, input_dim=7, activation=tf.nn.relu),
+    tf.keras.layers.Dense(64, activation=tf.nn.relu),
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 
-def _build_keras_model(**sk_params):
-    optimizer = tf.keras.optimizers.Adam(lr=0.01)
-    model.compile(loss=tf.losses.softmax_cross_entropy, optimizer=optimizer, metrics=['accuracy'])
+def _build_keras_model(**sk_params) -> tf.keras.Sequential:
+    optimizer = tf.keras.optimizers.Adam(lr=0.001)
+    model.compile(loss=tf.losses.sigmoid_cross_entropy ,optimizer=optimizer, metrics=['accuracy'])
     return model
 
-def build_keras_model(**sk_params):
+def build_keras_model(**sk_params) -> tf.keras.wrappers.scikit_learn.KerasClassifier:
     return tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=_build_keras_model, **sk_params)
